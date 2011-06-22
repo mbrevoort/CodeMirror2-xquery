84c0e0cc81d6708bfd5997871a05e650afa0d3bd
Mike Brevoort <mike@brevoort.com>
2011-06-21 23:02:11 -0600

    added xquery support for comments

--- mode/xquery/xquery.js
+++ mode/xquery/xquery.js
@@ -1,5 +1,62 @@
 CodeMirror.defineMode("xquery", function(config, parserConfig) {
 
+  // Used as scratch variables to communicate multiple values without
+  // consing up tons of objects.
+  var type, content;
+  function ret(tp, style, cont) {
+    type = tp; content = cont;
+    return style;
+  }
+  
+  function chain(stream, state, f) {
+    state.tokenize = f;
+    return f(stream, state);
+  }
+  
+  function tokenBase(stream, state) {
+    var ch = stream.next();
+    
+    if (ch == "(") {
+      if (stream.eat(":")) {
+        return chain(stream, state, tokenComment);
+      }
+    }
+    else {
+      stream.eatWhile(/[\w\$_]/);
+      return "word";
+    }
+  }
+
+  // handle the remainder of a comment until it's closed
+  function tokenComment(stream, state) {
+    var maybeEnd = false, ch;
+    while (ch = stream.next()) {
+      if (ch == ")" && maybeEnd) {
+        state.tokenize = tokenBase;
+        break;
+      }
+      maybeEnd = (ch == ":");
+    }
+    return ret("comment", "comment");
+  }
+
+
+  // the interface for the mode API
+  return {
+    startState: function(basecolumn) {
+      return {
+        tokenize: tokenBase
+      };
+    },
+
+    token: function(stream, state) {
+      if (stream.eatSpace()) return null;
+      var style = state.tokenize(stream, state);
+      if (type == "comment") return style;
+      return "word";
+    }
+  };
+
 });
 
 CodeMirror.defineMIME("application/xquery", "xquery");
