27e751ee7725a9da62002510c492c339140af679
Mike Brevoort <mikebre@ecollege.com>
2011-08-26 01:12:25 -0600

    added document as keyword, Closes #6

--- mode/xquery/xquery.js
+++ mode/xquery/xquery.js
@@ -50,7 +50,7 @@ CodeMirror.defineMode("xquery", function(config, parserConfig) {
     // {type: basic[i], style: "keyword"} e.g. 'after' --> {type: "after", style: "keyword"}
     var basic = ['after','ancestor','ancestor-or-self','and','as','ascending','assert','attribute','before',
     'by','case','cast','child','comment','declare','default','define','descendant','descendant-or-self',
-    'descending','document-node','element','else','eq','every','except','external','following',
+    'descending','document','document-node','element','else','eq','every','except','external','following',
     'following-sibling','follows','for','function','if','import','in','instance','intersect','item',
     'let','module','namespace','node','node','of','only','or','order','parent','precedes','preceding',
     'preceding-sibling','processing-instruction','ref','return','returns','satisfies','schema','schema-element',
